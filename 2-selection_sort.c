f45aad01a08a674c4e15c90bb9c8b6de08fd7c74
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-25 21:44:52 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,22 +7,29 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, idx_min;
-	int tmp;
+	int tmp,cont;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
 		idx_min = i;
+		cont = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[idx_min] > array[j])
 			{
 				idx_min = j;
+				cont++;
 			}
 		}
 
 		tmp = array[i];
 		array[i] = array[idx_min];
 		array[idx_min] = tmp;
-		print_array(array, size);
+
+		if (cont != 0)
+			print_array(array, size);
 	}
 }
