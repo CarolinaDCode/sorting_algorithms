16ecbdf876d8f9b187e40aabb846636c294a2365
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-26 20:50:58 -0700

    Quick sort

--- sort.h
+++ sort.h
@@ -22,4 +22,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+int partition(int *arr, int low, int high, size_t size);
+void sorting(int *arr, int low, int high, size_t size);
+
 #endif
